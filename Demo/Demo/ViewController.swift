ba6868965680069a3173a7a993645884cab4e437
Raksha Singh <raksha.singh@CardinalTSs-MacBook-Air.local>
2019-12-30 21:33:02 +0530

    Added Demo App

---
+++ Demo/Demo/ViewController.swift
@@ -0,0 +1,90 @@
+//
+//  ViewController.swift
+//  Demo
+//
+//  Created by Raksha Singh on 30/12/19.
+//  Copyright © 2019 Raksha Singh. All rights reserved.
+//
+
+import UIKit
+import Contacts
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var lbSuggestedNameOutlet: UILabel!
+    @IBOutlet weak var btn1: UIButton!
+    @IBOutlet weak var btn2: UIButton!
+    @IBOutlet weak var btn3: UIButton!
+    @IBOutlet weak var btn4: UIButton!
+    @IBOutlet weak var btn5: UIButton!
+    @IBOutlet weak var btn6: UIButton!
+    @IBOutlet weak var btn7: UIButton!
+    @IBOutlet weak var btn8: UIButton!
+    @IBOutlet weak var btn9: UIButton!
+    @IBOutlet weak var btn0: UIButton!
+    @IBOutlet weak var btnStra: UIButton!
+    @IBOutlet weak var btnHash: UIButton!
+    @IBOutlet weak var btnCall: UIButton!
+    
+    
+    @IBAction func btnAction(_ sender: Any) {
+        print((sender as AnyObject).tag!)
+        
+    }
+    
+    @IBAction func btnCallAction(_ sender: Any) {
+    }
+    
+    private func fetchContacts () {
+        print("Attempting contacts")
+        
+        let store = CNContactStore()
+        store.requestAccess(for: .contacts) { (granted, err) in
+            if let err = err {
+                print("Fail to request", err)
+                return
+            }
+            if granted
+            {
+                print("Access Granted...")
+                let keys = [CNContactGivenNameKey , CNContactPhoneNumbersKey , CNContactFamilyNameKey]
+                let request = CNContactFetchRequest(keysToFetch: keys as [CNKeyDescriptor])
+                do {
+                    try store.enumerateContacts(with: request, usingBlock:  { (contact, stopPointerIfYouWantToStopEnumeration) in
+                        print(contact.givenName)
+                        print(contact.familyName)
+                        print(contact.phoneNumbers.first?.value.stringValue ?? "")
+                    })
+                } catch let err {
+                    print("Fail to enumrt", err)
+                }
+                
+            } else {
+                print("Access Denied....")
+            }
+        }
+        
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        btn1.layer.cornerRadius = btn1.frame.size.width / 2
+        btn2.layer.cornerRadius = btn2.frame.size.width / 2
+        btn3.layer.cornerRadius = btn3.frame.size.width / 2
+        btn4.layer.cornerRadius = btn4.frame.size.width / 2
+        btn5.layer.cornerRadius = btn5.frame.size.width / 2
+        btn6.layer.cornerRadius = btn6.frame.size.width / 2
+        btn7.layer.cornerRadius = btn7.frame.size.width / 2
+        btn8.layer.cornerRadius = btn8.frame.size.width / 2
+        btn9.layer.cornerRadius = btn9.frame.size.width / 2
+        btn0.layer.cornerRadius = btn0.frame.size.width / 2
+        btnStra.layer.cornerRadius = btnStra.frame.size.width / 2
+        btnHash.layer.cornerRadius = btnHash.frame.size.width / 2
+        btnCall.layer.cornerRadius = btnCall.frame.size.width / 2
+        
+        fetchContacts ()
+        
+    }
+}
+
