14aa584c533d84f4aaa97afa0713ad3bad0e0cc0
Raksha Singh <raksha.singh@CardinalTSs-MacBook-Air.local>
2020-03-16 15:09:20 +0530

    Remove Click on main view while opened menu

--- MenuOptions/MenuOptions/ViewController.swift
+++ MenuOptions/MenuOptions/ViewController.swift
@@ -12,6 +12,7 @@ class ViewController: UIViewController {
 
     @IBOutlet weak var LeadingContraint: NSLayoutConstraint!
     
+    @IBOutlet weak var transparentView: UIView!
     @IBOutlet var mainView: UIView!
     @IBOutlet weak var menuView: UIView!
     
@@ -19,13 +20,40 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
-       
+        // Transparent View Intial Behaviour
+        transparentView.backgroundColor = .clear
+        transparentView.isHidden = true
+        
+       // Tap Gesture
+       let tap = UITapGestureRecognizer(target: self, action: #selector(handleTap(tap:)))
+       view.addGestureRecognizer(tap)
+       tap.numberOfTapsRequired = 1
+        
+        // Menu View Layer and Frames
         menuView.layer.borderWidth = 2
         menuView.layer.borderColor = #colorLiteral(red: 0.06274510175, green: 0, blue: 0.1921568662, alpha: 1)
+        
+        // Menu View LeadingContraint for appera When click
          LeadingContraint.constant = -200
     }
     
+    // Tap Gesture
+    @objc func handleTap(tap: UITapGestureRecognizer) {
+
+        guard tap.view != nil else { return }
+        
+        if tap.state == .ended {
+
+            LeadingContraint.constant = -200
+            UIView.animate(withDuration: 0.2 , delay: 0.0 , options: .curveEaseOut , animations: {
+                self.view.layoutIfNeeded()
+            })
+            transparentView.isHidden = true
+            menuShowing = !menuShowing
+        }
+    }
+    
+    // Click Menu Button Action
     @IBAction func openMenu(_ sender: Any) {
         
         if (menuShowing) {
@@ -33,12 +61,18 @@ class ViewController: UIViewController {
             UIView.animate(withDuration: 0.2 , delay: 0.0 , options: .curveEaseOut , animations: {
                 self.view.layoutIfNeeded()
             })
+            transparentView.backgroundColor = UIColor.brown
+            transparentView.backgroundColor = .clear
+            transparentView.isHidden = true
+
         }
         else {
             LeadingContraint.constant = 0
             UIView.animate(withDuration: 0.2 , delay: 0.0 , options: .curveEaseIn , animations: {
                 self.view.layoutIfNeeded()
             })
+            transparentView.backgroundColor = .clear
+            transparentView.isHidden = false
         }
         menuShowing = !menuShowing
     }
---
+++ TabBarController/TabBarController/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TabBarController
+//
+//  Created by Raksha Singh on 16/03/20.
+//  Copyright © 2020 Raksha Singh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
